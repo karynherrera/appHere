31341094c607430c06f008aaed06df0e27e583b0
Karyn Herrera <karynherrera@gmail.com>
2018-10-03 20:33:10 -0300

    se actualiza para funciones del menu

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -8,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class MenuComponent implements OnInit {
 
   constructor() { }
+  userClicks : Boolean = false;
 
   ngOnInit() {
   }
 
+  stateBtn(event){
+    console.log('hizo click');
+    }
 }

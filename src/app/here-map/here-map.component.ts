ed951da018d329b640b55b114fe1912b9c21217a
PaulaAraya <paula.arayawilliams@gmail.com>
2018-10-03 23:04:10 -0300

    geolocalizacion pinta posicion del usuario

--- src/app/here-map/here-map.component.ts
+++ src/app/here-map/here-map.component.ts
@@ -54,7 +54,8 @@ export class HereMapComponent implements OnInit {
       }
     );
     // tslint:disable-next-line:prefer-const
-    let behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(this.map));
+    this.displayCurrentPosition();
+    const behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(this.map));
     this.ui = H.ui.UI.createDefault(this.map, defaultLayers);
   }
 
@@ -81,6 +82,23 @@ export class HereMapComponent implements OnInit {
     }, false);
     this.map.addObject(marker);
   }
+
+  // Icono marcador de posicion de usario
+  displayCurrentPosition() {
+    // tslint:disable-next-line:max-line-length
+    const svgMarkup = '<svg aria-hidden="true" data-prefix="fas" data-icon="map-pin" width="30" height="30" class="svg-inline--fa fa-map-pin fa-w-9" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 288 512"><path fill="currentColor" d="M112 316.94v156.69l22.02 33.02c4.75 7.12 15.22 7.12 19.97 0L176 473.63V316.94c-10.39 1.92-21.06 3.06-32 3.06s-21.61-1.14-32-3.06zM144 0C64.47 0 0 64.47 0 144s64.47 144 144 144 144-64.47 144-144S223.53 0 144 0zm0 76c-37.5 0-68 30.5-68 68 0 6.62-5.38 12-12 12s-12-5.38-12-12c0-50.73 41.28-92 92-92 6.62 0 12 5.38 12 12s-5.38 12-12 12z"></path></svg>';
+    // tslint:disable-next-line:prefer-const
+    let svgIcon = new H.map.Icon(svgMarkup),
+      coords = {
+        lat: this.lat,
+        lng: this.lng
+      },
+      marker = new H.map.Marker(coords, {
+        icon: svgIcon
+      });
+    this.map.addObject(marker);
+  }
+
 }
 
 

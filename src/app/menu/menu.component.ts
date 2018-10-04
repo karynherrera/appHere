b616070ffdbc1e97835dc4747561d045bc5fcbb5
Karyn Herrera <karynherrera@gmail.com>
2018-10-04 18:06:22 -0300

    se vincula menu con mapa

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,28 +1,35 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef,Input } from '@angular/core';
 import { Router } from '@angular/router';
-import {HereMapComponent} from '../here-map/here-map.component';
+import { HereMapComponent } from '../here-map/here-map.component';
 
-declare var H: any;
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-  @ViewChild(HereMapComponent) map:HereMapComponent;
-  public mapElement: ElementRef;
+  @ViewChild(HereMapComponent) mapaHijo:HereMapComponent;
   
-
-  constructor(private router: Router) { this.query="";}
-  userClicks : Boolean = false;
-  query : string;
+  constructor(private router: Router, private mapis: HereMapComponent) {}
+  @Input() queryReceptor: string;
   
   ngOnInit() {
+    
+    //this.map.catchQuery('holi');
   }
 
-  stateBtn(event, name: string){
+  public stateBtn(event, name: string){
     console.log('hizo click '+name);
+    console.log(this.queryReceptor);
+    //this.map.catchQuery(name);
     //this.map.catchQuery(name);
     //this.router.navigate(['mapa']);
     }
+
+  catchQuery(value:string){
+    this.mapaHijo.catchQuery(value);
+    //this.mapaHijo.places(value);
+    //this.router.navigate(['mapa']);
+  }
 }
